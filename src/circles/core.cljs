4ada9563742acf852e3de5ed0a3538683bf1c349
Robert Mitchell <robert.mitchell36@gmail.com>
2020-12-01 08:57:21 -0600

    Add a minimal UI with no functionality
    - undo & redo buttons at the top as in the example
    - bordered canvas in which to draw circles

--- src/circles/core.cljs
+++ src/circles/core.cljs
@@ -2,6 +2,15 @@
   (:require [reagent.core :as r]
             [reagent.dom :as rd]))
 
+(defn circle-drawer []
+  [:div {:style {:display :flex :flex-direction :column}}
+   [:div {:style {:display :flex :justify-content :center}}
+    [:input {:type :button :value :Undo :style {:margin-right "15px"}}]
+    [:input {:type :button :value :Redo}]]
+   [:canvas {:style {:flex "1 1" :border "1px solid black"}
+             :width 500 :height 500
+             :id "circles-canvas"}]])
+
 (defn mount-root []
-  (rd/render [:h1 "Circles"]
+  (rd/render [circle-drawer]
              (js/document.getElementById "app-root")))
